9f680c79df15984f69c77fe9912b75f76dd379cb
supercellgamer <80987982+supercellgamer@users.noreply.github.com>
2021-05-16 14:00:29 -0600

    Add files via upload

---
+++ Zen Hub Test.lua
@@ -0,0 +1,17 @@
+
+
+print("                   -----------------------------------------------------------")
+print("                                               Zen Hub                        ")
+print("                                     Made by ", getgenv().creditsOne, "               ")
+print("                                          Made by ", getgenv().creditsTwo, "                    ")
+
+setclipboard("https://discord.gg/bJ9y4trTfm")
+
+print("                                  Copied discord link to clipboard            ")
+print("                             Discord server link: discord.gg/bJ9y4trTfm       ")
+print("                                                                              ")
+print("                                                                              ")
+print("                   -----------------------------------------------------------")
+print("                                                                              ")
+print("                                                                              ")
+print("                                                                              ")
